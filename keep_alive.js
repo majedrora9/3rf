dcdf1795db5e6ff5c651db67a4cf1d763e759595
Mohammad <153366997+majedrora9@users.noreply.github.com>
2024-06-25 20:37:47 +0300

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('I am alive!');
-}).listen(process.env.PORT || 3000, () => {
-  console.log('Server is listening on port ' + (process.env.PORT || 3000));
-});
